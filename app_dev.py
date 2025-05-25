bed9b87ab5a0d36017ea0002c93cf87f4e2d8f9e
B4k469420 <126454855+B4k469420@users.noreply.github.com>
2025-05-25 12:48:58 +0100

    Update app_dev.py

--- app_dev.py
+++ app_dev.py
@@ -63,6 +63,29 @@ st.markdown("""
         background-color: #3ba8d0;
         border-color: #3ba8d0;
     }
+
+      /* Remove blank space at top and bottom */ 
+    .block-container {
+       padding-top: 0rem;
+       padding-bottom: 0rem;
+    }
+   
+   /* Remove blank space at the center canvas */ 
+   .st-emotion-cache-z5fcl4 {
+       position: relative;
+       top: -62px;
+       }
+   
+   /* Make the toolbar transparent and the content below it clickable */ 
+   .st-emotion-cache-18ni7ap {
+       pointer-events: none;
+       background: rgb(255 255 255 / 0%)
+       }
+   .st-emotion-cache-zq5wmm {
+       pointer-events: auto;
+       background: rgb(255 255 255);
+       border-radius: 5px;
+       }
 </style>
 """, unsafe_allow_html=True)
 
