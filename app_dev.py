12536708b3990f84956fd9469454ede0911fce05
B4k469420 <126454855+B4k469420@users.noreply.github.com>
2025-05-27 19:50:46 +0100

    Update app_dev.py

--- app_dev.py
+++ app_dev.py
@@ -369,33 +369,34 @@ if not df.empty:
             if int(latest['pool_blocks_found']) == 68:
                 st.balloons()
                 st.markdown("""
-                <style>
-                @keyframes float {
-                    0% { transform: translateY(0); }
-                    100% { transform: translateY(-100vh); opacity: 0; }
-                }
-            
-                .emoji {
-                    position: fixed;
-                    bottom: 0;
-                    animation: float 5s ease-in infinite;
-                    font-size: 36px;
-                }
-            
-                .emoji:nth-child(1) { left: 10%; animation-delay: 0s; }
-                .emoji:nth-child(2) { left: 30%; animation-delay: 1s; }
-                .emoji:nth-child(3) { left: 50%; animation-delay: 2s; }
-                .emoji:nth-child(4) { left: 70%; animation-delay: 3s; }
-                .emoji:nth-child(5) { left: 90%; animation-delay: 4s; }
-                </style>
-            
-                <div class="emoji">🔥</div>
-                <div class="emoji">🎉</div>
-                <div class="emoji">6️⃣</div>
-                <div class="emoji">9️⃣</div>
-                <div class="emoji">🚀</div>
-                <div class="emoji">🍾</div>
-            """, unsafe_allow_html=True)
+                    <style>
+                    @keyframes float {
+                        0% { transform: translateY(0); }
+                        100% { transform: translateY(-100vh); opacity: 0; }
+                    }
+                
+                    .emoji {
+                        position: fixed;
+                        bottom: 0;
+                        animation: float 5s ease-in infinite;
+                        font-size: 36px;
+                    }
+                
+                    .emoji:nth-child(1) { left: 10%; animation-delay: 0s; }
+                    .emoji:nth-child(2) { left: 30%; animation-delay: 1s; }
+                    .emoji:nth-child(3) { left: 40%; animation-delay: 2s; }
+                    .emoji:nth-child(4) { left: 55%; animation-delay: 3s; }
+                    .emoji:nth-child(5) { left: 70%; animation-delay: 4s; }
+                    .emoji:nth-child(6) { left: 90%; animation-delay: 5s; }
+                    </style>
+                
+                    <div class="emoji">🔥</div>
+                    <div class="emoji">🎉</div>
+                    <div class="emoji">6️⃣</div>
+                    <div class="emoji">9️⃣</div>
+                    <div class="emoji">🚀</div>
+                    <div class="emoji">🍾</div>
+                """, unsafe_allow_html=True)
                 
             col1a, col1b = st.columns(2)
             with col1a: 
