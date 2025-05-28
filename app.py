5fa4267f48ddcb22859a8a6c78dae230eeff4007
B4k469420 <126454855+B4k469420@users.noreply.github.com>
2025-05-28 17:48:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -303,43 +303,6 @@ st.markdown("""
 </div>
 """, unsafe_allow_html=True)
 
-# HTML and CSS for floating and clickable image
-st.markdown(f"""
-    <style>
-    @keyframes floatCat {{
-        0% {{ transform: translate(0, 0); }}
-        100% {{ transform: translate({move_x}px, {move_y}px); }}
-    }}
-
-    .floating-cat {{
-        position: fixed;
-        top: {top}%;
-        left: {left}%;
-        width: 100px;
-        z-index: 9999;
-        animation: floatCat {duration}s ease-in-out infinite alternate;
-        cursor: pointer;
-        border-radius: 50%;
-        object-fit: cover;
-    }}
-
-    .cat-message {{
-        display: none;
-        position: fixed;
-        top: {top + 5}%;
-        left: {left + 5}%;
-        font-size: 20px;
-        background-color: #fff7dc;
-        padding: 10px 20px;
-        border-radius: 10px;
-        box-shadow: 0 0 10px #999;
-        z-index: 9999;
-    }}
-    </style>
-
-    <a href="https://matildaonqubic.com/" target="_blank">
-    <img src="data:image/png;base64,{encoded_cat}" class="floating-cat"
-       title="Hello! I'm Matilda the Satoshi’s Cat. In my idle time, I chase Monero blocks. 🐱" />
 
 # Metric Cards (Top Row)
 if not df.empty:
@@ -677,11 +640,51 @@ if not df.empty:
         with st.expander("📘 Competition Descriptions"):
             st.dataframe(desc_df)
 
-
 # Manual Refresh Button
 if st.button("🔄 Refresh Data", key="refresh"):
     st.cache_data.clear()
     st.rerun()
+
+if st.button("🐱 Release The Beast", key="beast"):
+    # HTML and CSS for floating and clickable image
+    st.markdown(f"""
+        <style>
+        @keyframes floatCat {{
+            0% {{ transform: translate(0, 0); }}
+            100% {{ transform: translate({move_x}px, {move_y}px); }}
+        }}
+    
+        .floating-cat {{
+            position: fixed;
+            top: {top}%;
+            left: {left}%;
+            width: 100px;
+            z-index: 9999;
+            animation: floatCat {duration}s ease-in-out infinite alternate;
+            cursor: pointer;
+            border-radius: 50%;
+            object-fit: cover;
+        }}
+    
+        .cat-message {{
+            display: none;
+            position: fixed;
+            top: {top + 5}%;
+            left: {left + 5}%;
+            font-size: 20px;
+            background-color: #fff7dc;
+            padding: 10px 20px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px #999;
+            z-index: 9999;
+        }}
+        </style>
+    
+        <a href="https://matildaonqubic.com/" target="_blank">
+        <img src="data:image/png;base64,{encoded_cat}" class="floating-cat"
+           title="Hello! I'm Matilda the Satoshi’s Cat. In my idle time, I chase Monero blocks. 🐱" />
+        </a>
+    """, unsafe_allow_html=True)
 
 
 # Footer
